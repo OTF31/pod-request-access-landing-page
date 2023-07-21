08744df356d20f4010ad9b9ed99c3cf847fa4132
Denis Omar Cuyo Ttito <75378049+OTF31@users.noreply.github.com>
2023-07-21 16:53:32 -0500

    add mobile design (#4)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,40 @@
-import { Title } from '@mantine/core';
+import { AppShell, BackgroundImage, Image, SimpleGrid, Stack, Text } from '@mantine/core';
+
+import applePodcast from '@/assets/desktop/apple-podcast.svg';
+import googlePodcasts from '@/assets/desktop/google-podcasts.svg';
+import logo from '@/assets/desktop/logo.svg';
+import pocketCasts from '@/assets/desktop/pocket-casts.svg';
+import spotify from '@/assets/desktop/spotify.svg';
+import imageHostMobile from '@/assets/mobile/image-host.jpg';
+import Button from '@/components/Button';
+import TextInput from '@/components/TextInput';
 
 const Home = () => {
-  return <Title>Pod request access landing page</Title>;
+  return (
+    <AppShell bg="riverStyx">
+      <BackgroundImage src={imageHostMobile} pos="absolute" inset={0} opacity={0.1} />
+      <Image src={logo} width={140} mx="auto" my={60} alt="logo" />
+      <Stack ta="center" c="black" fw={200}>
+        <Text color="screenGlow" fz={26}>
+          PUBLISH YOUR PODCASTS <Text color="white">EVERYWHERE.</Text>
+        </Text>
+        <Text color="periwinkleGrey">
+          Upload your audio to Pod with a single click. We'll then distribute your podcast
+          to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and more!
+        </Text>
+      </Stack>
+      <SimpleGrid cols={4} sx={{ alignItems: 'center' }} my={40}>
+        <Image src={spotify} alt="spotify" />
+        <Image src={applePodcast} alt="apple podcast" />
+        <Image src={googlePodcasts} alt="google podcasts" />
+        <Image src={pocketCasts} alt="pocket casts" />
+      </SimpleGrid>
+      <Stack>
+        <TextInput placeholder="Email address" />
+        <Button>Request Access</Button>
+      </Stack>
+    </AppShell>
+  );
 };
 
 export default Home;
