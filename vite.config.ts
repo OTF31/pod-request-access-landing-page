6d1192aea1db0d72a9e5ed61d787b03daa69c793
Denis Omar Cuyo Ttito <75378049+OTF31@users.noreply.github.com>
2023-07-22 18:32:46 -0500

    add configuration to deploy on GitHub Pages (#8)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  base: '/pod-request-access-landing-page/',
   plugins: [react(), svgr()],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
